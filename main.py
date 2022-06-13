37af46f004418825c9334df28f26b71545757f7e
DmitryBBB <ark-bykov@yandex.ru>
2022-06-13 20:11:30 +0300

    Lesson_3

--- main.py
+++ main.py
@@ -1,20 +1,4 @@
-from Lesson2.urls import Url
-from Lesson2.view import View
-from Lesson2.wsgi import Framework
-from Lesson2.response import Response
+from Lesson3.main import Framework
+from urls import routes, fronts
 
-
-class MyView(View):
-    def get(self, request):
-        return Response(body='GET SUCCESS')
-
-    def post(self, request):
-        return Response(status='201 Created', body='POST SUCCESS', headers={'MY_HEADERS': '222'})
-
-
-urls = [
-    Url('/page', MyView),
-
-]
-
-app = Framework(urls)
+app = Framework(routes, fronts)
