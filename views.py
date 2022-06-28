44292c81f0a98a1a2361cb2b5658be03bfdb027d
DmitryBBB <ark-bykov@yandex.ru>
2022-06-28 19:45:27 +0300

    Lesson_7 add architectural_system_pattern_unit_of_work.py; update creational_patterns.py and views; add create_db.py and create_db.sql

--- views.py
+++ views.py
@@ -2,13 +2,17 @@ from datetime import date
 
 from Lesson3.templator import render
 from patterns.behavioral_patterns import EmailNotifier, SmsNotifier, ListView, CreateView, BaseSerializer
-from patterns.creational_patterns import Engine, Logger
+from patterns.creational_patterns import Engine, Logger, MapperRegistry
 from patterns.stuctural_patterns import AppRoute, Debug
+from patterns.architectural_system_pattern_unit_of_work import UnitOfWork
 
 site = Engine()
 logger = Logger('main')
 email_notifier = EmailNotifier()
 sms_notifier = SmsNotifier()
+UnitOfWork.new_current()
+UnitOfWork.get_current().set_mapper_registry(MapperRegistry)
+
 
 routes = {}
 
@@ -174,6 +178,10 @@ class StudentListView(ListView):
     queryset = site.students
     template_name = 'student_list.html'
 
+    def get_queryset(self):
+        mapper = MapperRegistry.get_current_mapper('student')
+        return mapper.all()
+
 
 @AppRoute(routes=routes, url='/create-student/')
 class StudentCreateView(CreateView):
@@ -184,6 +192,8 @@ class StudentCreateView(CreateView):
         name = site.decode_value(name)
         new_obj = site.create_user('student', name)
         site.students.append(new_obj)
+        new_obj.mark_new()
+        UnitOfWork.get_current().commit()
 
 
 @AppRoute(routes=routes, url='/add-student/')
